c8ebfd67a72328a372230d5cce2dab20eed7730b
Sorikairo <sorikairox@gmail.com>
2018-04-02 17:22:57 +0200

    added mongoose paginate dependency and import

--- templates/model.js
+++ templates/model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
-
+var mongoosePaginate = require('mongoose-paginate');
 var {schemaName} = new Schema({fields});
-
+{schemaName}.plugin(mongoosePaginate);
 module.exports = mongoose.model('{modelName}', {schemaName});
--- templates/model.ts
+++ templates/model.ts
@@ -1,6 +1,8 @@
 import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+import mongoosePaginate from 'mongoose-paginate';
 
 const {schemaName} = new Schema({fields});
+{schemaName}.plugin(mongoosePaginate);
 
 export = mongoose.model('{modelName}', {schemaName});
