89fb18746107fbd25a1e32b8dc7ddd34a50757b4
Thomas Cruveilher <sorikairox@gmail.com>
2018-11-24 15:20:08 +0100

    added timestamps to models

--- templates/model.js
+++ templates/model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 var mongoosePaginate = require('mongoose-paginate');
-var {schemaName} = new Schema({fields});
+var {schemaName} = new Schema({fields}, { timestamps : true });
 {schemaName}.plugin(mongoosePaginate);
 module.exports = mongoose.model('{modelName}', {schemaName});
--- templates/model.ts
+++ templates/model.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 import mongoosePaginate from 'mongoose-paginate';
 
-const {schemaName} = new Schema({fields});
+const {schemaName} = new Schema({fields}, { timestamps : true });
 {schemaName}.plugin(mongoosePaginate);
 
 export = mongoose.model('{modelName}', {schemaName});
